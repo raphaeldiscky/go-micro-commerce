2c93b9d49a65774079d04408b5d9be8068ae9231
raphaeldiscky <raph.discky@gmail.com>
2025-08-28 13:47:16 +0700

    fix: grpc generation

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -5,14 +5,14 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 # Create output directory
-mkdir -p proto/marketplace/v1
+mkdir -p proto/
 
 # Generate Go code from proto files
 # Specify the directory containing your .proto files as the --proto_path
 # Then refer to the .proto files relative to that path
 protoc --proto_path=./proto \
-       --go_out=./proto/marketplace/v1 --go_opt=paths=source_relative \
-       --go-grpc_out=./proto/marketplace/v1 --go-grpc_opt=paths=source_relative \
+       --go_out=./proto --go_opt=paths=source_relative \
+       --go-grpc_out=./proto --go-grpc_opt=paths=source_relative \
        ./proto/*.proto
 
 echo "Protocol buffer files generated successfully!"
