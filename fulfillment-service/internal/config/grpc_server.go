136d5fc5030c017eed36482cad82692589906a5b
raphael discky <raph.discky@gmail.com>
2025-10-19 12:53:14 +0700

    feat: update fulfillment proto with connect rpc (#131)

--- fulfillment-service/internal/config/grpc_server.go
+++ fulfillment-service/internal/config/grpc_server.go
@@ -12,10 +12,11 @@ import (
 
 // GRPCServerConfig holds the configuration for the gRPC server.
 type GRPCServerConfig struct {
-	ServiceName string        `mapstructure:"GRPC_SERVER_SERVICE_NAME"`
-	Host        string        `mapstructure:"GRPC_SERVER_HOST"`
-	Port        int           `mapstructure:"GRPC_SERVER_PORT"`
-	GracePeriod time.Duration `mapstructure:"GRPC_SERVER_GRACE_PERIOD"`
+	ServiceName       string        `mapstructure:"GRPC_SERVER_SERVICE_NAME"`
+	Host              string        `mapstructure:"GRPC_SERVER_HOST"`
+	Port              int           `mapstructure:"GRPC_SERVER_PORT"`
+	GracePeriod       time.Duration `mapstructure:"GRPC_SERVER_GRACE_PERIOD"`
+	ReadHeaderTimeout time.Duration `mapstructure:"GRPC_SERVER_READ_HEADER_TIMEOUT"`
 }
 
 // initGRPCServerConfig initializes the gRPC server configuration from environment variables.
@@ -25,6 +26,7 @@ func initGRPCServerConfig() *GRPCServerConfig {
 	viper.SetDefault("GRPC_SERVER_HOST", "0.0.0.0")
 	viper.SetDefault("GRPC_SERVER_PORT", constant.GRPCPort)
 	viper.SetDefault("GRPC_SERVER_GRACE_PERIOD", constant.GRPCGracePeriod)
+	viper.SetDefault("GRPC_SERVER_READ_HEADER_TIMEOUT", constant.GRPCReadHeaderTimeout)
 
 	grpcServerConfig := &GRPCServerConfig{}
 	if err := viper.Unmarshal(grpcServerConfig); err != nil {
--- proto/go.mod
+++ proto/go.mod
@@ -5,13 +5,7 @@ go 1.24.7
 require (
 	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.9-20250912141014-52f32327d4b0.1
 	connectrpc.com/connect v1.19.0
-	google.golang.org/grpc v1.75.0
 	google.golang.org/protobuf v1.36.9
 )
 
-require (
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
-)
+require github.com/google/go-cmp v0.7.0 // indirect
